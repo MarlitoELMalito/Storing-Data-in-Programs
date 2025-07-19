684f27ab2f625b771f254120af52ac96bfa46d98
MarlitoELMalito <marlomarlohaha1@gmail.com>
2025-07-18 19:41:33 -0700

    Add files via upload

---
+++ Storing Data in Programs.py
@@ -0,0 +1,6 @@
+name = "Marlo"
+age = "19"
+house_number = "1429"
+street_name = "pacific"
+
+print(f"This is {name}. They are {age} years old and live at house number {house_number} on {street_name}.")
